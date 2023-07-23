73e1b9f6369d5695bd5153d00669f4b3a3a577ce
Sreten <sretenstankov@gmail.com>
2023-07-23 16:17:48 +0200

    Init

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    ChromeDriver driver;
+
+    public ChromeDriver otvoriPretrazivac() {
+
+        ChromeDriver driver = new ChromeDriver();
+        driver.navigate().to("https://www.saucedemo.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        return driver;
+    }
+}
