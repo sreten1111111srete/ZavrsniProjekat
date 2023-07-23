9195de60b018ce074f777129663649101ac3e048
Sreten <sretenstankov@gmail.com>
2023-07-23 17:55:57 +0200

    Testiranje logovanja

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -11,8 +11,8 @@ public class BaseTest {
         ChromeDriver driver = new ChromeDriver();
         driver.navigate().to("https://www.saucedemo.com/");
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(50));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
 
         return driver;
     }
