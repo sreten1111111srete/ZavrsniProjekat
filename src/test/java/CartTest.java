188f69a1335a03b3023aa179eefdc6617b29b5ee
Sreten <sretenstankov@gmail.com>
2023-07-24 14:00:30 +0200

    Testiranje checkout dugmeta

---
+++ src/test/java/CartTest.java
@@ -0,0 +1,39 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CartTest extends BaseTest {
+
+    LoginPage loginPage;
+    InventoryPage inventoryPage;
+    CartPage cartPage;
+
+    @BeforeMethod
+    public void priprema() {
+
+        driver = otvoriPretrazivac();
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        cartPage = new CartPage(driver);
+        loginPage.ulogujSe("standard_user", "secret_sauce");
+        inventoryPage.sortirajPoNajnizojCeni();
+        inventoryPage.dodajArtikal1();
+        inventoryPage.dodajArtikal2();
+        inventoryPage.dodajArtikal3();
+        inventoryPage.klikniKorpu();
+    }
+
+    @Test
+    public void testCheckout() {
+
+        cartPage.klikCheckout();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/checkout-step-one.html");
+    }
+
+    @AfterMethod
+    public void zatvori() {
+        driver.quit();
+    }
+}
