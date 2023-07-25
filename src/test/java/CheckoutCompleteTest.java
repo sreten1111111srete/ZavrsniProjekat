1a9aa6ec4748eccf1d92a555e3c23bb4bfc716b4
Sreten <sretenstankov@gmail.com>
2023-07-25 12:14:55 +0200

    Kraj kupovine

---
+++ src/test/java/CheckoutCompleteTest.java
@@ -0,0 +1,44 @@
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class CheckoutCompleteTest extends BaseTest {
+
+    LoginPage loginPage;
+    InventoryPage inventoryPage;
+    CartPage cartPage;
+    CheckoutStepOnePage checkoutStepOnePage;
+    CheckoutCompletePage checkoutCompletePage;
+
+    @BeforeMethod
+    public void priprema() {
+
+        driver = otvoriPretrazivac();
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
+        cartPage = new CartPage(driver);
+        checkoutStepOnePage = new CheckoutStepOnePage(driver);
+        checkoutCompletePage = new CheckoutCompletePage(driver);
+        loginPage.ulogujSe("standard_user", "secret_sauce");
+        inventoryPage.sortirajPoNajnizojCeni();
+        inventoryPage.dodajArtikal1();
+        inventoryPage.dodajArtikal2();
+        inventoryPage.dodajArtikal3();
+        inventoryPage.klikniKorpu();
+        cartPage.klikniCheckout();
+        checkoutStepOnePage.popuniFormular("Sreten", "Stankov", "11070");
+    }
+
+    @Test
+    public void krajKupovine() {
+        checkoutCompletePage.klikni();
+
+        Assert.assertEquals(checkoutCompletePage.zavrsnaPoruka(), "Thank you for your order!");
+    }
+
+    @AfterMethod
+    public void zatvori() {
+        driver.quit();
+    }
+}
